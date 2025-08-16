d29f2948aea20fa56bba0257922208129555bbb3
Sawit Pooh <sawit.j@kkumail.com>
2025-08-17 03:35:12 +0700

    feat: add quest

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "home/index"
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
+  post "/quests", to: "home#create", as: :quests
 end
