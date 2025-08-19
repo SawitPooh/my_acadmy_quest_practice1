77bfc022c59b6c474464fc203beb311508bf78f3
Sawit Pooh <sawit.j@kkumail.com>
2025-08-19 23:02:03 +0700

    feat: my brag doccument

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post   "/quests",                  to: "home#create",         as: :quests
   patch  "/quests/:id/toggle_status", to: "home#toggle_status", as: :toggle_status_quest
   delete "/quests/:id",              to: "home#destroy",        as: :quest
+
+  get "/home/brag",                   to: "home#brag",           as: :brag
 end
